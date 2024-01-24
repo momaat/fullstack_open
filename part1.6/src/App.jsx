2ac8af67d4c469c0f84525e165f63f20c2a08265
momaat <monica.tuominen@gmail.com>
2024-01-24 22:33:01 +0200

    Update 1.10

--- part1.6/src/App.jsx
+++ part1.6/src/App.jsx
@@ -21,8 +21,9 @@ const Statistics = (props) => {
       <StatisticLine text="good" value ={props.good} />
       <StatisticLine text="neutral" value ={props.neutral} />
       <StatisticLine text="bad" value ={props.bad} />
+      <StatisticLine text="all" value ={props.good+props.neutral+props.bad} />
       <StatisticLine text="average" value ={props.average} />
-      <StatisticLine text="positive" value ={props.positive} />
+      <StatisticLine text="positive" value ={props.positive+" %"}/>
     </div>
   )
 }
