1250d956bc4f2e9a4fa667ea126c46c9077f8ebe
momaat <monica.tuominen@gmail.com>
2024-01-24 22:36:25 +0200

    Add 1.11

--- part1.6/src/App.jsx
+++ part1.6/src/App.jsx
@@ -18,12 +18,16 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <StatisticLine text="good" value ={props.good} />
-      <StatisticLine text="neutral" value ={props.neutral} />
-      <StatisticLine text="bad" value ={props.bad} />
-      <StatisticLine text="all" value ={props.good+props.neutral+props.bad} />
-      <StatisticLine text="average" value ={props.average} />
-      <StatisticLine text="positive" value ={props.positive+" %"}/>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value ={props.good} />
+          <StatisticLine text="neutral" value ={props.neutral} />
+          <StatisticLine text="bad" value ={props.bad} />
+          <StatisticLine text="all" value ={props.good+props.neutral+props.bad} />
+          <StatisticLine text="average" value ={props.average} />
+          <StatisticLine text="positive" value ={props.positive+" %"}/>
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -31,7 +35,10 @@ const Statistics = (props) => {
 const StatisticLine = (props) => {
   return (
     <>
-     <p>{props.text} {props.value}</p>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
     </>
   )
 }
