e581a8b06d07b12d9bbafd7c80edb7f47a416240
momaat <monica.tuominen@gmail.com>
2024-01-26 13:09:11 +0200

    Add 2.2

--- part2/2.1_courseinfo/src/components/Content.jsx
+++ part2/2.1_courseinfo/src/components/Content.jsx
@@ -1,12 +1,21 @@
 import Part from "./Part"
 
+
+
 const Content = ({parts}) => {
+
+    var totalAmount = parts.reduce((sum, part) => sum + part.exercises, 0)
+
     return (
-        <ul>
-            {parts.map(part => 
-                <Part key={part.id} part={part.name} exercises={part.exercises} />
-            )}
-        </ul>
+        <div>
+            <ul>
+                {parts.map(part => 
+                    <Part key={part.id} part={part.name} exercises={part.exercises} />
+                )}
+            </ul>
+            <p><strong>Total of {totalAmount} exercises</strong></p>
+            
+        </div>
     )
 }
 
