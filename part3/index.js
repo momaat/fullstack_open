2a9e37a6be05a0190e70f22189673d643ddfcc5f
momaat <monica.tuominen@gmail.com>
2024-03-08 11:06:57 +0200

    Add 3.1

---
+++ part3/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+      },
+      {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+      },
+      {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+      }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+app.get('/api/persons', (request, response) => {
+response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
