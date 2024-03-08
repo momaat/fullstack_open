78988d77a8db571687a858c6cae4481396cbbafa
momaat <monica.tuominen@gmail.com>
2024-03-08 13:33:43 +0200

    Add 3.5

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -34,6 +36,7 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${personsAmount} people</p><p>${date}</p>`)
 })
 
+// 3.3 Get person with id
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
@@ -45,11 +48,50 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+// 3.4 Delete person
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+// 3.5 Add person
+const generateId = () => {
+  // const maxId = persons.length > 0
+  //   ? Math.max(...persons.map(p => p.id))
+  //   : 0
+  // return maxId + 1
+  const id = Math.floor(Math.random() * 9999)
+
+  return id
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if(!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if(!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+
 })
 
 const PORT = 3001
